15095bc7435e5a752b6c8c1327686b1f170d9e5e
Suraj Sanchit <suraj@graciousstudios.nl>
2017-08-16 01:00:03 +0200

    Add filename prefix

--- index.js
+++ index.js
@@ -6,29 +6,31 @@ var stringify = require('css-stringify');
 var cleanUpString = require('clean-up-string');
 var dirname = require('path').dirname;
 var pathjoin = require('path').join;
+var basename = require('path').basename;
 
-module.exports = function() {
-	return through.obj(function(file, enc, callback) {
-		var stream = this;
-		var reworkData = rework(file.contents.toString())
-			.use(reworkMoveMedia());
-		var stylesheets = split(reworkData);
-		var stylesheetKeys = Object.keys(stylesheets);
-		stylesheetKeys.forEach(function(key) {
-			var poop = file.clone({
-				contents: false
-			});
-			var name = cleanUpString(key);
-			var contents = stringify(stylesheets[key]);
-			poop.contents = new Buffer(contents);
-			if (name) {
-				var filepath = pathjoin(dirname(file.path), name + '.css');
-				poop.path = filepath;
-			} else {
-				poop.path = file.path;
-			}
-			stream.push(poop);
-		});
-		callback();
-	});
+module.exports = function () {
+    return through.obj(function (file, enc, callback) {
+        var stream = this;
+        var reworkData = rework(file.contents.toString())
+            .use(reworkMoveMedia());
+        var stylesheets = split(reworkData);
+        var stylesheetKeys = Object.keys(stylesheets);
+        stylesheetKeys.forEach(function (key) {
+            var poop = file.clone({
+                contents: false
+            });
+            var name = cleanUpString(key);
+            var contents = stringify(stylesheets[key]);
+            poop.contents = new Buffer(contents);
+            if (name) {
+                var filename = basename(file.path, '.css') + '-' + name + '.css';
+                var filepath = pathjoin(dirname(file.path), filename);
+                poop.path = filepath;
+            } else {
+                poop.path = file.path;
+            }
+            stream.push(poop);
+        });
+        callback();
+    });
 };
